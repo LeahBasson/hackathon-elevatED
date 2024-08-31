d9ccf8ca106beaed47f7209572be4b6a2c35b0e9
LeahBasson <leahbasson54@gmail.com>
2024-08-31 21:08:23 +0200

    :sparkles:

--- script/code.js
+++ script/code.js
@@ -8,3 +8,30 @@ window.addEventListener('scroll', () =>{
         navEl.classList.remove('navbar-scrolled');
     }
 })
+
+ // Function to validate footer form field
+ function validateFooterForm() {
+    let footerForm = document.forms["footerForm"];
+    let footerEmail = footerForm["footerEmail"].value;
+  
+    if (footerEmail === "") {
+        document.getElementById("footer_error").innerHTML = "Please enter your email address";
+        return false; // Prevent form submission
+      } 
+      else {
+        document.getElementById("footer_error").innerHTML = ""; 
+        }
+
+        return true; // Allow form submission if validation passes
+}
+
+  // Get the footer form element
+  let footerForm = document.forms["footerForm"];
+
+  // Attach the event listener to the footer form's submit event
+  footerForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    if (validateFooterForm()) {
+      event.target.submit();
+    }
+  });
